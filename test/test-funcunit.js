6cc84c67122bc9ce39674a652bf56e2f612bad78
Marshall Thompson <marshall@creativeideal.net>
2017-03-17 13:24:26 -0600

    Separate funcunit and unit tests

--- test/test-funcunit.js
+++ test/test-funcunit.js
@@ -2,14 +2,6 @@ import assert from 'chai/chai';
 import mocha from 'steal-mocha';
 import F from 'funcunit';
 
-// import 'wallet-ui/models/test';
-// import 'wallet-ui/components/page-signup/page-signup-test';
-// import 'wallet-ui/components/page-home/page-home-test';
-// import 'wallet-ui/components/page-portfolio/page-portfolio-test';
-// import 'wallet-ui/components/page-login/page-login-test';
-// import 'wallet-ui/components/page-settings/page-settings-test';
-// import 'wallet-ui/components/page-four-oh-four/page-four-oh-four-test';
-
 F.attach(mocha);
 
 // Note: don't use arrow functions if you need to access `this` (see https://mochajs.org/#arrow-functions).
