35e23325f869b911638b84c7fc87fd1cecf25049
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-23 18:33:25 -0400

    adding placeholders for crypto methods

--- src/components/page-auth/page-auth.js
+++ src/components/page-auth/page-auth.js
@@ -2,6 +2,7 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import view from './page-auth.stache';
 import feathersClient from '~/models/feathers-client-rest';
+import route from 'can-route';
 
 export const ViewModel = DefineMap.extend({
   email: {
@@ -30,6 +31,8 @@ export const ViewModel = DefineMap.extend({
     })
     .then(response => {
       Object.assign(this, response);
+      // If its a new account we want to generate 12 words of the recovery phrase and a Private Key.
+      // Both 12 words and PK will be encrypted with a password and saved as user data in DB.
     })
     .catch(error => {
       console.log(error);
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,35 @@
 import DefineMap from 'can-define/map/';
 import DefineList from 'can-define/list/list';
-import feathersClient from './feathers-client';
-// import feathersClient from './feathers-client-rest';
+//import feathersClient from './feathers-client';
+import feathersClient from './feathers-client-rest';
 import superModel from './super-model';
 import algebra from './algebra';
 
 var User = DefineMap.extend('User', {
   _id: 'string',
   email: 'string',
-  password: 'string'
+  password: 'string',
+  usedTmpPassword: {
+    type: 'boolean',
+    value: false
+  },
+  isNewAccount: {
+    type: 'boolean',
+    value: false
+  },
+  generatePrivateKey(){
+    // To generate Private Key we need to generate 12 words first.
+  },
+  signWithPrivateKey(){
+    // Transactions and messages will be signed with PrivateKey.
+  },
+  encryptWithPassword(){
+    // Private key and the 12 recovery words should be encrypted with user password.
+  },
+  decryptWithPassword(){
+    // To sign anything with PrivateKey we need to decrypt it.
+    // Also we want allow user to save his recovery phrase which also can be decrypted with pswd.
+  }
 });
 
 User.List = DefineList.extend('UserList', {
