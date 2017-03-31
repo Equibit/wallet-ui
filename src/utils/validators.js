cb77adabca82ccdfbc0870bd61df7670bab402c6
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-31 16:24:42 -0400

    login form validation

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,13 +1,12 @@
 const emailRegex = /.+@.+\..+/i;
 
 export default {
-  email (value, {allowEmpty}) {
+  email (value, { allowEmpty }) {
     return (!allowEmpty && !value && 'Email is missing') ||
-      (value && !emailRegex.test(value) && 'Enter a valid email address') ||
-      '';
+      (value && !emailRegex.test(value) && 'Enter a valid email address') || '';
   },
-  password (value) {
-    return (!value && value !== null) && 'Password is missing';
+  password (value, { allowEmpty }) {
+    return (!allowEmpty && !value && 'Password is missing') || '';
   },
   terms (value) {
     return (!value && 'You need to read and agree to our Terms & Conditions and Privacy Policy') || '';
