12048d5388985eef2a32d29fcb9138e09da893a6
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-30 23:09:04 -0400

    password-strength: updated test cases and message labels

--- src/components/page-auth/password-strength/test.js
+++ src/components/page-auth/password-strength/test.js
@@ -4,24 +4,24 @@ import { ViewModel } from './password-strength';
 
 describe('wallet-ui/components/page-auth/password-strength', function () {
   const vm = new ViewModel({});
-  it('should detect very weak password', function () {
-    vm.password = 'test';
+  it('should detect Worst password', function () {
+    vm.password = 'zxcvbn';
     assert.equal(vm.score, 0);
   });
-  it('should detect weak password', function () {
-    vm.password = 'mypass123';
+  it('should detect Bad password', function () {
+    vm.password = 'love88';
     assert.equal(vm.score, 1);
   });
-  it('should detect medium password', function () {
-    vm.password = 'mypass123P';
+  it('should detect Weak password', function () {
+    vm.password = 'zhang198822';
     assert.equal(vm.score, 2);
   });
-  it('should detect strong password', function () {
-    vm.password = 'mypass123Psw';
+  it('should detect Good password', function () {
+    vm.password = 'neverforget13/3/1997';
     assert.equal(vm.score, 3);
   });
-  it('should detect very strong password', function () {
-    vm.password = 'mypass123Psw@#';
+  it('should detect Strong password', function () {
+    vm.password = 'briansmith4mayor';
     assert.equal(vm.score, 4);
   });
 });
