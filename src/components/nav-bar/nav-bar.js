8efdc69964a37ee8311596dfd68aaa5602ee43cc
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-07 16:27:45 -0400

    added nav-bar component

---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,16 @@
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import './nav-bar.less';
+import view from './nav-bar.stache';
+
+export const ViewModel = DefineMap.extend({
+  message: {
+    value: 'This is the nav-bar component'
+  }
+});
+
+export default Component.extend({
+  tag: 'nav-bar',
+  ViewModel,
+  view
+});
