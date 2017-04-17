e8a8fc200083c8be09b489ec7000041378be9194
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-17 10:16:27 -0400

    docs: nav-bar

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,14 +1,22 @@
 /**
  *
- * @module {can.Component} components/nav-bar Global Nav
+ * @module {can.Component} components/nav-bar <nav-bar>
  * @parent components.common
+ *
+ * Global navbar.
+ *
+ * @signature `<nav-bar {page}="page" {logout}="@logout" {is-logged-in}="isLoggedIn">`
+ *
+ *  @param {String} page A reference to the route param `page`.
+ *  @param {Function} logout A function to be called on logout button click.
+ *  @param {Boolean} isLoggedIn Navbar has two states logged in and anonymous to show hide relevant controls.
  *
  * @link ../src/components/nav-bar/nav-bar.html Full Page Demo
  * ## Example
  *
  * @demo src/components/nav-bar/nav-bar.html
  *
-**/
+ */
 
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
