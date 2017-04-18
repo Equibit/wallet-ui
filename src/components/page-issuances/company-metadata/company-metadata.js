29c8a1b7960dc8077de126a9193d098def1987c1
Adri <adri@bitovi.com>
2017-04-18 10:03:53 -0500

    Styles for cards and wells

--- src/components/page-issuances/company-metadata/company-metadata.js
+++ src/components/page-issuances/company-metadata/company-metadata.js
@@ -1,3 +1,14 @@
+/**
+ * @module {can.Component} components/page-issuances/company-metadata company-metadata
+ * @parent components.issuances
+ *
+ * @link ../src/components/page-issuances/company-metadata/company-metadata.html Full Page Demo
+ * ## Example
+ *
+ * @demo src/components/page-issuances/company-metadata/company-metadata.html
+ *
+**/
+
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './company-metadata.less';
