549eacdceb300c43c9c9233f405767761041928d
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-25 13:57:48 -0400

    added candlestick chart to issuance details page

--- src/components/page-issuance-details/page-issuance-details.js
+++ src/components/page-issuance-details/page-issuance-details.js
@@ -2,32 +2,10 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './page-issuance-details.less';
 import view from './page-issuance-details.stache';
-// import $ from 'jquery';
 
 export const ViewModel = DefineMap.extend({
   message: {
     value: 'This is the page-issuance-details component'
-  },
-  chartData: {
-    type: '*'
-  },
-  chartHeight: {
-    value: 250
-  },
-  candlestickSize: {
-    set (val) {
-      setTimeout(() => {
-        this.loadData(val);
-      }, 0);
-      return val;
-    }
-  },
-  loadData (candlestickSize) {
-    console.log(`loadData(${candlestickSize})`);
-    // $.get(candlestickSize < 1000 ? 'data-1.json' : 'data-2.json').then(chartData => {
-    //   console.log(`loadedData(${chartData.length})`);
-    //   this.chartData = chartData;
-    // });
   }
 });
 
---
+++ src/models/candlestick.js
@@ -0,0 +1,51 @@
+/**
+ * @module {can-map} models/candlestick Candlestick
+ * @parent models
+ *
+ * User model
+ *
+ * @group models/candlestick.properties 0 properties
+ * @group models/candlestick.prototype 1 prototype
+ * @group models/candlestick.static 2 static
+ */
+
+import DefineMap from 'can-define/map/';
+import DefineList from 'can-define/list/list';
+import superModel from '~/models/super-model';
+import feathersClient from '~/models/feathers-client';
+import algebra from '~/models/algebra';
+
+// FIXTURES!!!
+import '~/models/fixtures/candlestick';
+
+const Candlestick = DefineMap.extend('Candlestick', {
+  /**
+   * @property {String} models/candlestick.properties.date date
+   * @parent models/candlestick.properties
+   * Date timestamp, in seconds.
+   */
+  'date': 'number',
+  'high': 'number',
+  'low': 'number',
+  'open': 'number',
+  'close': 'number',
+  'volume': 'number',
+  'quoteVolume': 'number',
+  'weightedAverage': 'number'
+});
+
+Candlestick.List = DefineList.extend('CandlestickList', {
+  '#': Candlestick
+});
+
+Candlestick.connection = superModel({
+  Map: Candlestick,
+  List: Candlestick.List,
+  feathersService: feathersClient.service('/candlestick'),
+  name: 'users',
+  algebra
+});
+
+Candlestick.algebra = algebra;
+
+export default Candlestick;
