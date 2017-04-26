26b70030f9890285adbeccaca23e422bb6f6f4fb
Marshall Thompson <marshall@creativeideal.net>
2017-04-26 15:42:38 -0600

    Use basic session auth for an app password

--- start.js
+++ start.js
@@ -2,15 +2,37 @@ const feathers = require('feathers');
 const path = require('path');
 const host = process.env.VIRTUAL_HOST || process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 8080;
-const basicAuth = require('express-basic-auth');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
 const handler = require('feathers-errors/handler');
 const notFound = require('feathers-errors/not-found');
 
 const app = feathers();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(basicAuth({
-  users: { 'admin': process.env.HTTP_PASSWORD }
+app.use(expressSession({
+  secret: '49df-2395-98nb-d5f6',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
 }));
+
+app.post('/basic-login', function (req, res) {
+  if (!req.body.password) {
+    res.send('login failed');
+  } else if (req.body.password === process.env.HTTP_PASSWORD) {
+    req.session.authorized = true;
+    res.redirect('/');
+  }
+});
+
+app.use(function (req, res, next) {
+  if (!req.session.authorized) {
+    return res.sendFile(path.join(__dirname, './basic-login.html'));
+  }
+  return next();
+});
 
 // Host the public folder
 app.use('/', feathers.static('./'));
