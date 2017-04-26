b2c5ad430e5763ae384daed6b20c8ee2a1a11227
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-26 11:18:35 -0400

    panel: do not pass title and add panel-heading html, use full content

--- src/components/page-issuance-details/page-issuance-details.js
+++ src/components/page-issuance-details/page-issuance-details.js
@@ -2,10 +2,21 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './page-issuance-details.less';
 import view from './page-issuance-details.stache';
+import Issuance from '~/models/issuance';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the page-issuance-details component'
+  issuance: {
+    value: new Issuance({
+      issuanceName: 'Series 1',
+      issuanceType: 'Common Shares',
+      restriction: '1',
+      volume24h: 1541,
+
+      sharesAuthorized: 10000,
+      sharesIssued: 8459,
+      sharesDividend: 764,
+      sharesDividendYield: 2.6,
+    })
   }
 });
 
