a2d6f46263c18fc822aae14507c24834c1dac267
Marshall Thompson <marshall@creativeideal.net>
2017-04-26 09:18:00 -0600

    Use HOST variable

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const server = require('pushstate-server');
-const host = process.env.VIRTUAL_HOST || '127.0.0.1';
+const host = process.env.VIRTUAL_HOST || process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 8080;
 
 server.start({
