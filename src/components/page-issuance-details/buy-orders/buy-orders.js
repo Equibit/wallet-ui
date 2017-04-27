5c9b4a2491873aafd8f23ddf9356c356fdaf5976
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-27 02:27:44 -0400

    updated book order components

--- src/components/page-issuance-details/buy-orders/buy-orders.js
+++ src/components/page-issuance-details/buy-orders/buy-orders.js
@@ -17,10 +17,21 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './buy-orders.less';
 import view from './buy-orders.stache';
+import _ from 'lodash';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the buy-orders component'
+  rows: {
+    value: _.times(10, (i) => {
+      return {
+        quantity: 100 * i,
+        price: 70 * i,
+        date: '13:47',
+        partial: ['YES', 'NO'][i % 2]
+      };
+    })
+  },
+  sell (id) {
+    return;
   }
 });
 
--- src/components/page-issuance-details/sell-orders/sell-orders.js
+++ src/components/page-issuance-details/sell-orders/sell-orders.js
@@ -19,8 +19,18 @@ import './sell-orders.less';
 import view from './sell-orders.stache';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the sell-orders component'
+  rows: {
+    value: _.times(10, (i) => {
+      return {
+        quantity: 100 * i,
+        price: 70 * i,
+        date: '13:47',
+        partial: ['YES', 'NO'][i % 2]
+      };
+    })
+  },
+  buy (id) {
+    return;
   }
 });
 
