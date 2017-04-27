da1d0b90763be5f485ec999462edd7c9d1533224
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-27 13:09:51 -0400

    added custom generator templates for component

---
+++ .donejs/templates/component/modlet/component.js
@@ -0,0 +1,31 @@
+/**
+ * @module {can.Component} <%= module %> <%= tag %>
+ * @parent components.common
+ *
+ * A short description of the <%=tag %> component
+ *
+ * @signature `<<%=tag %> />`
+ *
+ * @link ../src/<%= module %>.html Full Page Demo
+ *
+ * ## Example
+ *
+ * @demo src/<%= module %>.html
+ */
+
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import './<%= name %>.less';
+import view from './<%= name %>.stache';
+
+export const ViewModel = DefineMap.extend({
+  message: {
+    value: 'This is the <%= tag %> component'
+  }
+});
+
+export default Component.extend({
+  tag: '<%= tag %>',
+  ViewModel,
+  view
+});
