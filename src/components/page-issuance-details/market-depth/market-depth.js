8ac23938f4080f91cf54b9a2cc5d2ebe8ea3ce53
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-16 00:49:36 -0400

    market-depth chart initial version, no real data

--- src/components/page-issuance-details/market-depth/market-depth.js
+++ src/components/page-issuance-details/market-depth/market-depth.js
@@ -15,12 +15,60 @@
 
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import DefineList from 'can-define/list/list';
+import _ from 'lodash';
 import './market-depth.less';
 import view from './market-depth.stache';
 
+let data1 = { key: "ask", type: "area-spline", data: new DefineList(
+  _.times(10, i => Math.abs(Math.floor(Math.sin(i) * 30))).concat(_.times(9, () => null))
+  ) },
+  data2 = { key: "bid", type: "area-spline", data: new DefineList(
+    _.times(9, () => null).concat(_.times(10, i => Math.abs(Math.floor(Math.sin(2*i) * 30))))
+  ) };
+
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the market-depth component'
+  dataColumns: {
+    get (lastVal, resolve) {
+      setTimeout(() => {
+        resolve(new DefineList([data1, data2]))
+      }, 100)
+    }
+  },
+  config: {
+    value: {
+      color: {
+        pattern: ['#32B576', '#EC2F39']
+      },
+      height: 30,
+      axis: {
+        // x1: {
+        //   categories: _.times(19, i => (i * 30))
+        // },
+        y: {
+          tick: {
+            count: 4,
+            format: (d) =>  Math.floor(d)
+          }
+        }
+      },
+      grid: {
+        x: {
+          show: true
+        },
+        y: {
+          show: true
+        }
+      },
+      size: {
+        height: 140
+      },
+      padding: {
+        left: 30,
+        right: 10,
+        bottom: 10
+      }
+    }
   }
 });
 
