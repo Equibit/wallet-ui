4e73220561eed418cd053204c82271433d5a49fa
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-17 18:30:40 -0400

    watchlist: load issuances for now; styling. Donejs templates: added doctype for demo pages

--- src/components/page-portfolio/watch-list/watch-list.js
+++ src/components/page-portfolio/watch-list/watch-list.js
@@ -17,10 +17,18 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './watch-list.less';
 import view from './watch-list.stache';
+import Issuance from '~/models/issuance';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the watch-list component'
+  rowsPromise: {
+    get () {
+      return Issuance.getList({$limit: 10, $skip: 0});
+    }
+  },
+  rows: {
+    get (val, resolve) {
+      this.rowsPromise.then(resolve);
+    }
   }
 });
 
