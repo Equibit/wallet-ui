f8d97ac6a4bb2297b2e8b155daa9dd0dc1247594
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-24 11:44:36 -0400

    added test to test suite

--- src/components/test.js
+++ src/components/test.js
@@ -2,3 +2,4 @@ import '~/components/page-auth/password-strength/test';
 import '~/components/common/alerts/alerts-test';
 
 import '~/components/page-issuances/company-metadata/company-metadata-test';
+import '~/components/trade-funds/currency-converter/currency-converter-test'
