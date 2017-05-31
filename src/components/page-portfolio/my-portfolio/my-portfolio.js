858685b3875591e95b2af69628aa51a99eb649c5
Marshall Thompson <marshall@creativeideal.net>
2017-05-31 12:09:41 -0600

    portfolio value is initially null

--- src/components/page-portfolio/my-portfolio/my-portfolio.js
+++ src/components/page-portfolio/my-portfolio/my-portfolio.js
@@ -20,13 +20,14 @@ import view from './my-portfolio.stache';
 
 export const ViewModel = DefineMap.extend({
   portfolio: {
-    value: {
-      balance: 22.616393,
-      totalCash: 12.616393,
-      totalSec: 10.00045,
-      unrealizedPL: 0.109,
-      unrealizedPLPercent: 1.2
-    }
+    value: null
+    // value: {
+    //   balance: 22.616393,
+    //   totalCash: 12.616393,
+    //   totalSec: 10.00045,
+    //   unrealizedPL: 0.109,
+    //   unrealizedPLPercent: 1.2
+    // }
   },
   isSendFundsPopup: 'boolean',
   isReceiveFundsPopup: 'boolean',
