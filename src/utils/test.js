3e09258a580e5fa16092b281872cf65c0e2b4aa4
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-06-01 16:44:54 -0400

    fixing tests

--- src/utils/test.js
+++ src/utils/test.js
@@ -12,9 +12,9 @@ describe('utils/crypto', function () {
     assert.ok(seed instanceof Uint8Array, 'seed should be an array of 8-bit unsigned integers');
     assert.equal(seed.length, 64, 'seed should be 8 * 64 = 512 bit');
   });
-  it('mnemonicToPrivateKey', function () {
+  it('mnemonicToHDNode', function () {
     let mnemonic = crypto.generateMnemonic();
-    let pk = crypto.mnemonicToPrivateKey(mnemonic);
+    let pk = crypto.mnemonicToHDNode(mnemonic);
     assert.ok(pk.chainCode instanceof Uint8Array, 'chainCode should be an array of 8-bit unsigned integers');
     assert.ok(pk.keyPair.compressed, 'keyPair should be compressed');
   });
