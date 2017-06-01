e831987366f1014cd0614c6ce23434b14e2d8bbb
Adri <adri@bitovi.com>
2017-06-01 12:05:08 -0500

    Connected components on the order-data component

---
+++ src/components/common/status-badge/status-badge-test.js
@@ -0,0 +1,10 @@
+import assert from 'chai/chai';
+import 'steal-mocha';
+import { ViewModel } from './status-badge';
+
+describe('wallet-ui/components/common/status-badge', function () {
+  it('should have message', function () {
+    var vm = new ViewModel();
+    assert.equal(vm.message, 'This is the status-badge component');
+  });
+});
