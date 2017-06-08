3c688954989718d2b6cdfe2f291e7d350380cf9f
Adri <adri@bitovi.com>
2017-06-08 11:24:34 -0500

    styles for notifications popover

--- src/components/notifications/notifications-popover/notifications-popover.js
+++ src/components/notifications/notifications-popover/notifications-popover.js
@@ -2,7 +2,8 @@
  * @module {can.Component} components/notifications/notifications-popover notifications-popover
  * @parent components.notifications
  *
- * Shows a list of alerts (`<notifications-list>`) inside of a popover.
+ * Shows a list of alerts (`<notifications-list>`) inside of a popover that uses the [dropdown](components__common__dropdown.html)
+ * component.
  *
  * @signature `<notifications-popover />`
  *
