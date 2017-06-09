c6ec62825ae2757b6fc617a22a77913c8f7587ea
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-06-09 13:21:15 -0400

    portfolio: added nextAddress, import a new address as watch-only

--- src/models/portfolio.js
+++ src/models/portfolio.js
@@ -41,6 +41,29 @@ const Portfolio = DefineMap.extend('Portfolio', {
     type: 'string'
   },
 
+  /**
+   * @property {String} models/portfolio.properties.addresses addresses
+   * @parent models/portfolio.properties
+   * Tracking addresses by index for one-time usage.
+   * ```
+   * {
+   *   eqb: {
+   *     0: {used: true},
+   *     1: {used: false}
+   *   },
+   *   btc: {
+   *     0: {used: false}
+   *   }
+   * }
+   * ```
+   */
+  // TODO: consider to simplify structure using hd path: `{"m'/44'/0/0/0": {used: true}}`
+  addresses: {
+    // TODO: update when service gets implemented.
+    //serialize: true,
+    type: '*'
+  },
+
   index: 'number',
   balance: {type: 'number', value: 0},
   totalCash: {type: 'number', value: 0},
@@ -60,14 +83,58 @@ const Portfolio = DefineMap.extend('Portfolio', {
   // "m /44' /0' /portfolio-index' /0 /index"
   nextAddress: {
     get () {
-      // TODO: check this.addresses and return the last unused address.
-      return {
-        btc: this.keys.btc.derive(0).derive(0).getAddress(),
-        eqb: this.keys.eqb.derive(0).derive(0).getAddress()
+      const btcAddr = getNextAddressIndex(this.addresses, 'btc');
+      const eqbAddr = getNextAddressIndex(this.addresses, 'eqb');
+      const addr = {
+        btc: this.keys.btc.derive(0).derive(btcAddr.index).getAddress(),
+        eqb: this.keys.eqb.derive(0).derive(eqbAddr.index).getAddress()
+      };
+      if (btcAddr.imported === false) {
+        // Import addr as watch-only
+        this.importAddr(addr.btc);
+        // Mark address as generated/imported but not used yet:
+        this.addresses.btc[btcAddr.index] = {used: false};
       }
+      if (eqbAddr.imported === false) {
+        // Import addr as watch-only
+        this.importAddr(addr.eqb);
+        // Mark address as generated/imported but not used yet:
+        this.addresses.eqb[eqbAddr.index] = {used: false};
+      }
+
+      return addr;
     }
+  },
+
+  // Methods:
+  /**
+   * @function models/portfolio.properties.importAddr importAddr
+   * @parent models/portfolio.properties
+   * Imports the given address to the built-in wallet (to become watch-only for registering transactions).
+   */
+  importAddr (addr) {
+    // TODO: replace with a specific service (e.g. /import-address).
+    feathersClient.service('proxycore').find({
+      query: {
+        method: 'importaddress',
+        params: [addr]
+      }
+    }).then(res => {
+      if (!res.error) {
+        console.log('The address was imported successfully', res);
+      } else {
+        console.error('There was an error when I tried to import your address: ', res);
+      }
+    });
   }
 });
+
+function getNextAddressIndex (addresses, type=btc) {
+  const addrByType = addresses[type];
+  return Object.keys(addrByType).reduce((acc, i) => {
+    return addrByType[i].used !== true ? {index: Number(i), imported: true} : {index: Number(i) + 1, imported: false};
+  }, {index: 0, imported: false});
+}
 
 Portfolio.List = DefineList.extend('PortfolioList', {
   '#': Portfolio
@@ -84,3 +151,10 @@ Portfolio.connection = superModel({
 Portfolio.algebra = algebra;
 
 export default Portfolio;
+export { getNextAddressIndex };
+
+// Import an address to be added as watch-only to the built-in wallet:
+// http://localhost:3030/proxycore?method=importaddress&params[]=mwd7FgMkm9yfPmNTnntsRbugZS7BEZaf32
+
+// List unspent TOs for given addresses:
+// http://localhost:3030/proxycore?method=listunspent&params[0]=0&params[1]=99999&params[2][]=mp9GiieHrLQvLu4C8nE9bbwxNmXqcC3nVf&params[2][]=mwd7FgMkm9yfPmNTnntsRbugZS7BEZaf32
