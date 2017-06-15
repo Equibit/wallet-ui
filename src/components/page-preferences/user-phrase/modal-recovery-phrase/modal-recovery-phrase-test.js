16547d79036bcd5becde7ce9e9cbe419a8900061
Adri <adri@bitovi.com>
2017-06-15 13:21:23 -0500

    recovery phrase components

---
+++ src/components/page-preferences/user-phrase/modal-recovery-phrase/modal-recovery-phrase-test.js
@@ -0,0 +1,10 @@
+import assert from 'chai/chai';
+import 'steal-mocha';
+import { ViewModel } from './modal-recovery-phrase';
+
+describe('wallet-ui/components/page-preferences/user-phrase/modal-recovery-phrase', function () {
+  it('should have message', function () {
+    var vm = new ViewModel();
+    assert.equal(vm.message, 'This is the modal-recovery-phrase component');
+  });
+});
---
+++ src/components/page-preferences/user-phrase/modal-recovery-phrase/phrase-input/phrase-input.js
@@ -0,0 +1,31 @@
+/**
+ * @module {can.Component} wallet-ui/components/page-preferences/user-phrase/modal-recovery-phrase/phrase-input phrase-input
+ * @parent components.user-preferences
+ *
+ * Shows 4 fields at random where the recovery phrase can be verified.
+ *
+ * @signature `<phrase-input />`
+ *
+ * @link ../src/wallet-ui/components/page-preferences/user-phrase/modal-recovery-phrase/phrase-input/phrase-input.html Full Page Demo
+ *
+ * ## Example
+ *
+ * @demo src/wallet-ui/components/page-preferences/user-phrase/modal-recovery-phrase/phrase-input/phrase-input.html
+ */
+
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import './phrase-input.less';
+import view from './phrase-input.stache';
+
+export const ViewModel = DefineMap.extend({
+  message: {
+    value: 'This is the phrase-input component'
+  }
+});
+
+export default Component.extend({
+  tag: 'phrase-input',
+  ViewModel,
+  view
+});
