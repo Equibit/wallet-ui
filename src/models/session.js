b119f9053c91b661b77c63f5c3036f65484022de
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-06-21 19:16:23 -0400

    use lowercased currency type for query

--- src/models/session.js
+++ src/models/session.js
@@ -112,8 +112,8 @@ const Session = DefineMap.extend('Session', {
       const hasAddr = this.allAddresses && (this.allAddresses.BTC.length || this.allAddresses.EQB.length);
       return hasAddr && feathersClient.service('/listunspent').find({
         query: {
-          BTC: this.allAddresses.BTC,
-          EQB: this.allAddresses.EQB,
+          btc: this.allAddresses.BTC,
+          eqb: this.allAddresses.EQB,
           byaddress: true
         }
       });
