a82e432f5362f73f04fd07ce9b219c8a40d4cc7b
Adri <adri@bitovi.com>
2017-08-14 17:16:18 -0500

    fix for build

--- src/components/page-my-issuances/issuance-list/issuance-list-test.js
+++ src/components/page-my-issuances/issuance-list/issuance-list-test.js
@@ -5,6 +5,6 @@ import { ViewModel } from './issuance-list'
 describe('wallet-ui/components/page-my-issuances/issuance-list', function () {
   it('should have message', function () {
     var vm = new ViewModel()
-    assert.equal(vm.message, 'This is the issuance-list component');
+    assert.equal(vm.message, 'This is the issuance-list component')
   })
 })
