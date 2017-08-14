afec622aaad03b5fa20bae8d0b94a1570b683ed4
Adri <adri@bitovi.com>
2017-08-14 11:12:12 -0500

    removing semicolons to fix build

--- src/components/common/entity-info/entity-info-test.js
+++ src/components/common/entity-info/entity-info-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './entity-info';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './entity-info'
 
 describe('wallet-ui/components/common/entity-info', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
-    assert.equal(vm.message, 'This is the entity-info component');
+    var vm = new ViewModel()
+    assert.equal(vm.message, 'This is the entity-info component')
   });
 });
