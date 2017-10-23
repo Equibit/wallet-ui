f27a561fa204d3ff9c71d8dde973b5618fb37a35
Adri <adri@bitovi.com>
2017-10-23 16:00:15 -0500

    Added cases for no passports, received passports and owned passports

--- src/components/page-trading-passports/trading-passports-list/trading-passports-list-test.js
+++ src/components/page-trading-passports/trading-passports-list/trading-passports-list-test.js
@@ -5,6 +5,6 @@ import { ViewModel } from './trading-passports-list'
 describe('wallet-ui/components/page-trading-passports/trading-passports-list', function () {
   it('should have message', function () {
     var vm = new ViewModel()
-    assert.equal(vm.message, 'This is the trading-passports-list component');
+    assert.equal(vm.message, 'This is the trading-passports-list component')
   })
 })
