2d64bfdb9e096b99072fec32176d7f140d883564
Adri <adri@bitovi.com>
2017-10-25 14:54:10 -0500

    removed semicolons

--- src/components/page-trading-passports/create-passport/passport-confirm/passport-confirm-test.js
+++ src/components/page-trading-passports/create-passport/passport-confirm/passport-confirm-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './passport-confirm';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './passport-confirm'
 
 describe('wallet-ui/components/page-trading-passports/create-passport/passport-confirm', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
+    var vm = new ViewModel()
     assert.equal(vm.message, 'This is the passport-confirm component');
-  });
-});
+  })
+})
--- src/components/page-trading-passports/create-passport/passport-form/passport-form-test.js
+++ src/components/page-trading-passports/create-passport/passport-form/passport-form-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './passport-form';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './passport-form'
 
 describe('wallet-ui/components/page-trading-passports/create-passport/passport-form', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
-    assert.equal(vm.message, 'This is the passport-form component');
-  });
-});
+    var vm = new ViewModel()
+    assert.equal(vm.message, 'This is the passport-form component')
+  })
+})
