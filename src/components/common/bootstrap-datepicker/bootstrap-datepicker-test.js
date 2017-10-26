e0214ec98ac2fadd9aef57f473e3ce43685aa008
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-26 12:12:53 -0400

    added common/bootstrap-datepicker

---
+++ src/components/common/bootstrap-datepicker/bootstrap-datepicker-test.js
@@ -0,0 +1,10 @@
+import assert from 'chai/chai';
+import 'steal-mocha';
+import { ViewModel } from './bootstrap-datepicker';
+
+describe('wallet-ui/components/common/bootstrap-datepicker', function () {
+  it('should have message', function () {
+    var vm = new ViewModel();
+    assert.equal(vm.message, 'This is the bootstrap-datepicker component');
+  });
+});
