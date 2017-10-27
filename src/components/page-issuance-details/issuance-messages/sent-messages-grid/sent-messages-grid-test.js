7f16d6b35fc5caf597b29e78eabc49d381e8ce9c
Adri <adri@bitovi.com>
2017-10-27 11:54:43 -0500

    Added sent messages grid component

---
+++ src/components/page-issuance-details/issuance-messages/sent-messages-grid/sent-messages-grid-test.js
@@ -0,0 +1,10 @@
+import assert from 'chai/chai';
+import 'steal-mocha';
+import { ViewModel } from './sent-messages-grid';
+
+describe('wallet-ui/components/page-issuance-details/issuance-messages/sent-messages-grid', function () {
+  it('should have message', function () {
+    var vm = new ViewModel();
+    assert.equal(vm.message, 'This is the sent-messages-grid component');
+  });
+});
