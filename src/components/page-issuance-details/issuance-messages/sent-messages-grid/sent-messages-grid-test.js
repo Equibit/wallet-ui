cfaa59d6064445ccf4107c6eb5b78d56793eb6aa
Adri <adri@bitovi.com>
2017-10-27 13:14:58 -0500

    removed semicolons

--- src/components/page-issuance-details/issuance-messages/sent-messages-grid/sent-messages-grid-test.js
+++ src/components/page-issuance-details/issuance-messages/sent-messages-grid/sent-messages-grid-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './sent-messages-grid';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './sent-messages-grid'
 
 describe('wallet-ui/components/page-issuance-details/issuance-messages/sent-messages-grid', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
-    assert.equal(vm.message, 'This is the sent-messages-grid component');
-  });
-});
+    var vm = new ViewModel()
+    assert.equal(vm.message, 'This is the sent-messages-grid component')
+  })
+})
