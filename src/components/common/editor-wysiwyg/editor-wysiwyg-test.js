4be478e6cceef675370a4f6009c1122270621b93
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-11-02 10:50:32 -0400

    linting

--- src/components/common/editor-wysiwyg/editor-wysiwyg-test.js
+++ src/components/common/editor-wysiwyg/editor-wysiwyg-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './editor-wysiwyg';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './editor-wysiwyg'
 
 describe('wallet-ui/components/common/editor-wysiwyg', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
-    assert.equal(vm.message, 'This is the editor-wysiwyg component');
-  });
-});
+    var vm = new ViewModel()
+    assert.equal(vm.message, 'This is the editor-wysiwyg component')
+  })
+})
