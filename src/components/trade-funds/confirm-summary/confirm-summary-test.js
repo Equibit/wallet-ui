8ee2c22e79a1f931a4ff29d4c3568dd0de05f5ef
Adri <adri@bitovi.com>
2018-01-16 10:22:00 -0600

    removed commas

--- src/components/trade-funds/confirm-summary/confirm-summary-test.js
+++ src/components/trade-funds/confirm-summary/confirm-summary-test.js
@@ -1,10 +1,10 @@
-import assert from 'chai/chai';
-import 'steal-mocha';
-import { ViewModel } from './confirm-summary';
+import assert from 'chai/chai'
+import 'steal-mocha'
+import { ViewModel } from './confirm-summary'
 
 describe('wallet-ui/components/trade-funds/confirm-summary', function () {
   it('should have message', function () {
-    var vm = new ViewModel();
-    assert.equal(vm.message, 'This is the confirm-summary component');
-  });
-});
+    var vm = new ViewModel()
+    assert.equal(vm.message, 'This is the confirm-summary component')
+  })
+})
