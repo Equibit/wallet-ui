3c8330d48588c3f92dc7f53e811e6dde7f28fb92
Hinson Lo <hinsonl@evenset.com>
2018-06-14 12:11:25 -0400

    Add better safety check for formatter.

--- src/utils/stache-helpers/format.js
+++ src/utils/stache-helpers/format.js
@@ -40,14 +40,14 @@ stache.registerHelper('format-int', function (value) {
 // 5000.1 => 5,000.10
 // 5000 => 5,000
 stache.registerHelper('format-coin', function (value, precision) {
-  if (isNaN(value)) return value
+  if (isNaN(value) && typeof value !== 'number') return value
 
   if (typeof precision !== 'number') {
     precision = 2
   }
 
   let formattedValue = accounting.formatNumber(value)
-  if (value % 1 !== 0) {
+  if (value && value % 1 !== 0) {
     value = toMaxPrecision(value, precision)
     const exactPrecision = value.toString().split('.')[1].length
     formattedValue = accounting.formatMoney(value, '', exactPrecision)
