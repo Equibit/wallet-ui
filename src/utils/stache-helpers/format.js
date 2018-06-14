097983115d1d64a215801a13fa850109d98840d3
Hinson Lo <hinsonl@evenset.com>
2018-06-14 12:26:49 -0400

    Remove NaN check.

--- src/utils/stache-helpers/format.js
+++ src/utils/stache-helpers/format.js
@@ -40,7 +40,7 @@ stache.registerHelper('format-int', function (value) {
 // 5000.1 => 5,000.10
 // 5000 => 5,000
 stache.registerHelper('format-coin', function (value, precision) {
-  if (isNaN(value) && typeof value !== 'number') return value
+  if (typeof value !== 'number') return value
 
   if (typeof precision !== 'number') {
     precision = 2
