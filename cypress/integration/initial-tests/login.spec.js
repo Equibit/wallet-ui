ad8211348dcd34e5c09ae705ddb9a7dc12d3e5ce
Elliot Tio <elliott@evenset.com>
2018-07-17 16:30:44 -0400

    login test

--- cypress/integration/initial-tests/login.spec.js
+++ cypress/integration/initial-tests/login.spec.js
@@ -1,17 +1,13 @@
-describe('Login Test', function() {
-    it('successfully loads', function() {
-      // cy.visit('https://qa-wallet.equibitgroup.com')
-      cy.visit('/portfolio')
-      cy.get('input[name=username]').should('exist')
-      // cy.visit('/login')
-      // cy.contains('This Page is Password Protected')
-
-      // cy.get('.form-control')
-      //   .type('bullish')
-      //   .should('have.value', 'bullish')
-      
-      // cy.contains('Submit').click()
-      
-      // cy.contains('Login')
+describe('Login Test', () => {
+    it('successfully log in', () => {
+      cy.visit('/')
+      .get('input[type="email"]')
+      .type('elliott@evenset.com')
+      .get('input[type="password"]')
+      .type('SpicyTunaMayo')
+      .get('button[type="submit"]').click()
+        .location().should((loc) => {
+          expect(loc.pathname).to.eq('/portfolio')
+        })
     })
   })
