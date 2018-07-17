f9f315b920e3ce545d54790072ae49301fbf47e6
Elliot Tio <elliott@evenset.com>
2018-07-17 17:51:19 -0400

    first CI attempt

--- cypress/integration/initial-tests/login.spec.js
+++ cypress/integration/initial-tests/login.spec.js
@@ -1,10 +1,10 @@
 describe('Login Test', () => {
     it('successfully log in', () => {
       cy.visit('/')
-      .get('input[type="email"]')
+      .get('input[type="email"]', { timeout: 20000 })
       .type('elliott@evenset.com')
       .get('input[type="password"]')
-      .type('SpicyTunaMayo')
+      .type('password123')
       .get('button[type="submit"]').click()
         .location().should((loc) => {
           expect(loc.pathname).to.eq('/portfolio')
