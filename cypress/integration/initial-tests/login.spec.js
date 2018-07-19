fbe14fc11dc0c6f9a9a07c3618ba67343a2f8ab0
Elliot Tio <elliott@evenset.com>
2018-07-19 11:34:22 -0400

    configure travis integration

--- cypress/integration/initial-tests/login.spec.js
+++ cypress/integration/initial-tests/login.spec.js
@@ -1,15 +1,109 @@
+// import User from '../../../src/models/user/user'
+
 /*eslint-disable */
 describe('Login Test', () => {
-  it('successfully log in', () => {
+
+  beforeEach(() => {
+    cy.fixture('users').as('users')
     cy.visit('/')
+    // sign up programmatically
+    // User.signup(users[0].email)
+  })
+
+  afterEach(() => {
+    // remove each entry
+  })
+
+  // test banner, test greeting, test inputs, test button
+  // test error states - blank email, blank password, invalid email/password
+  // test url redirect after success
+
+  it.skip('greets user with Log In', () => {
+    cy.contains('h2', 'Log In', { timeout: 20000 })
+  })
+
+  it.skip('links to Sign Up flow', () => {
+    cy
+    .contains('Sign Up', { timeout: 20000 })
+    .should('have.attr', 'href', '/signup')
+  })
+
+  it.skip('links to Forget Password flow', () => {
+    cy
+    .contains('Forgot Password?', { timeout: 20000 })
+    .should('have.attr', 'href', '/forgot-password')
+  })
+
+  it.skip('requires email', () => {
+    cy
+    .get('input[type="password"]', { timeout: 20000 })
+    .type('password{enter}')
+
+    cy
+    .get('.form-text')
+    .should('contain', 'Email is missing')
+  })
+
+  it.skip('requires password', () => {
+    cy
     .get('input[type="email"]', { timeout: 20000 })
+    .type('test@evenset.com{enter}')
+
+    cy
+    .get('.form-text')
+    .should('contain', 'Password is missing')
+  })
+
+  it.skip('requires valid email', () => {
+    cy
+    .get('input[type="email"]', { timeout: 20000 })
+    .type('test')
+    .get('input[type="password"]')
+    .type('password{enter}')
+
+    cy
+    .get('.form-text')
+    .should('contain', 'Enter a valid email address')
+  })
+
+  it.skip('requires valid email and password', () => {
+    cy
+    .get('input[type="email"]', { timeout: 20000 })
+    .type('test@evenset.com')
+    .get('input[type="password"]')
+    .type('password')
+
+    cy
+    .get('button[type="submit"]').click()
+
+    cy
+    .get('.alert')
+    .should('contain', 'Invalid login.')
+  })
+
+  it('successfully log in using UI', function () {
+
+    cy.get('input[type="email"]', { timeout: 20000 })
     .type('elliott@evenset.com')
     .get('input[type="password"]')
     .type('password123')
     .get('button[type="submit"]').click()
-      .location().should((loc) => {
-        expect(loc.pathname).to.eq('/portfolio')
-      })
+    
+    cy.url().should('contain', '/portfolio')
+  })
+
+  it.skip('successfully log in programmatically', function () {
+    cy.request({
+      method: 'POST',
+      url: '/login',
+      form: true,
+      body: {
+        email: this.users[0].email,
+        password: this.users[0].password
+      }
+    })
+
+    cy.url().should('include', '/portfolio')
   })
 })
 /*eslint-enable */
