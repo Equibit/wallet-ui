da2b402df6da3b7c734eb06e75ee983b20beb6ba
Elliot Tio <elliott@evenset.com>
2018-07-19 14:57:08 -0400

    first login test

--- testcafe-tests/login.js
+++ testcafe-tests/login.js
@@ -1,15 +1,21 @@
-import { Selector } from 'testcafe';
+/*eslint-disable */
+import { Selector } from 'testcafe'
 
 fixture `Getting Started`
     // .page `http://localhost:8080/`
     .page `qa-wallet.equibitgroup.com`
 
-const inputEmail = Selector('.form-control')
-const loginHeader = Selector('.form-signin-heading')
+const inputEmail = Selector('#inputEmail').with({timeout: 20000})
+const inputPassword = Selector('#inputPassword').with({timeout: 20000})
+const panelTitle = Selector('.panel-title')
 
-test('My First Test', async t => {
+test('Successfully log in', async t => {
     await t
-        .typeText(inputEmail, 'bullish')
-        .click('.btn')
-        .expect(loginHeader.innerText).contains('Log In')
+        .typeText('.form-control', 'bullish')
+        .click('button[type="submit"]')
+        .typeText(inputEmail, 'elliott@evenset.com')
+        .typeText(inputPassword, 'password123')
+        .click('button[type="submit"]')
+        .expect(panelTitle.innerText).contains('My Portfolio', 'Title contains expected title')
 })
+/*eslint-enable */
