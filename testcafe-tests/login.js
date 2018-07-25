75e432131847e90d25c1e328019cd4610f7f39ba
Elliot Tio <elliott@evenset.com>
2018-07-25 17:58:34 -0400

    added more tests

--- testcafe-tests/login.js
+++ testcafe-tests/login.js
@@ -10,15 +10,100 @@ fixture `Getting Started`
             .click('button[type="submit"]')
     })
 
-const inputEmail = Selector('#inputEmail').with({timeout: 20000})
-const inputPassword = Selector('#inputPassword').with({timeout: 20000})
-const getPageUrl = ClientFunction(() => window.location.href);
+test('greets user with Log In', async t => {
+    const header = Selector('.form-signin-heading').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+    })
 
-test('Successfully log in', async t => {
+    await t
+        .expect(header.innerHTML)
+        .contains('Log In', 'header does not contain log in')
+})
+
+test.skip('links to Sign Up flow', async t => {
+    const signUp = Selector('a[href="/signup"]')
+
+    await t
+        .expect(signUp)
+        .getAttribute('href')
+        .contains('/signup', 'sign up link found')
+})
+
+test.skip('links to Forget Password flow', async t => {
+    const forgotPassword = Selector('.btn btn-link pull-right')
+
+    await t
+        .expect(forgotPassword)
+        .getAttribute('href')
+        .contains('/forgot-password', 'forgot password link not found')
+})
+
+test('requires email', async t => {
+    const password = Selector('#inputPassword')
+    const error = Selector('.form-text').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+    })
+
+    await t
+        .typeText(password, 'testpassword')
+        .click('button[type="submit"]')
+        .expect(error.innerHTML)
+        .contains('Email is missing', 'missing email error not found')
+})
+
+test('requires password', async t => {
+    const email = Selector('#inputEmail')
+    const error = Selector('.form-text').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+    })
+
+    await t
+        .typeText(email, 'test@evenset.com')
+        .click('button[type="submit"]')
+        .expect(error.innerHTML)
+        .contains('Password is missing', 'missing password error not found')
+})
+
+test('requires valid email', async t => {
+    const email = Selector('#inputEmail')
+    const password = Selector('#inputPassword')
+    const error = Selector('.form-text').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+    })
+
+    await t
+        .typeText(email, 'test')
+        .typeText(password, 'testpassword')
+        .click('button[type="submit"]')
+        .expect(error.innerHTML)
+        .contains('Enter a valid email address', 'invalid email error not found')
+})
+
+test('requires valid email and password', async t => {
+    const email = Selector('#inputEmail')
+    const password = Selector('#inputPassword')
+    const error = Selector('.alert').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+    })
+
+    await t
+        .typeText(email, 'test@evenset.com')
+        .typeText(password, 'testpassword')
+        .click('button[type="submit"]')
+        .expect(error.innerHTML)
+        .contains('Invalid login.', 'invalid login alert not found')
+})
+
+test('successfully log in using UI', async t => {
+    const inputEmail = Selector('#inputEmail').with({timeout: 20000})
+    const inputPassword = Selector('#inputPassword').with({timeout: 20000})
+    const getPageUrl = ClientFunction(() => window.location.href)
+
     await t
         .typeText(inputEmail, 'test@evenset.com')
         .typeText(inputPassword, 'SpicyTunaMayo')
         .click('button[type="submit"]')
-        .expect(getPageUrl()).contains('portfolio')
+        .expect(getPageUrl())
+        .contains('portfolio')
 })
 /*eslint-enable */
