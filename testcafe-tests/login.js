90414a87ed33be46ca0a80d1598c2d3c40439c7b
Elliot Tio <elliott@evenset.com>
2018-07-25 16:44:27 -0400

    added beforeEach hook, added travis script to install dependencies

--- testcafe-tests/login.js
+++ testcafe-tests/login.js
@@ -4,6 +4,11 @@ import { Selector, ClientFunction } from 'testcafe'
 fixture `Getting Started`
     .page `http://localhost:8080/`
     // .page `qa-wallet.equibitgroup.com`
+    .beforeEach( async t => {
+        await t
+            .typeText('.form-control', 'bullish')
+            .click('button[type="submit"]')
+    })
 
 const inputEmail = Selector('#inputEmail').with({timeout: 20000})
 const inputPassword = Selector('#inputPassword').with({timeout: 20000})
@@ -11,8 +16,6 @@ const getPageUrl = ClientFunction(() => window.location.href);
 
 test('Successfully log in', async t => {
     await t
-        .typeText('.form-control', 'bullish')
-        .click('button[type="submit"]')
         .typeText(inputEmail, 'test@evenset.com')
         .typeText(inputPassword, 'SpicyTunaMayo')
         .click('button[type="submit"]')
