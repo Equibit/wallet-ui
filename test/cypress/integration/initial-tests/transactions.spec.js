5d1c47835f78109d32e5e20a6004bc8df9a23635
Elliot Tio <elliott@evenset.com>
2018-08-14 11:16:53 -0400

    hard stash

--- test/cypress/integration/initial-tests/transactions.spec.js
+++ test/cypress/integration/initial-tests/transactions.spec.js
@@ -20,14 +20,14 @@ describe('Transactions Test', () => {
         cy.contains('Equibit').click()
         cy.contains('Next').click()
         cy
-        .get('.form-text.help-block')
-        .should('contain', 'Address is missing')
+          .get('.form-text.help-block')
+          .should('contain', 'Address is missing')
     
         cy.contains('Bitcoin').click()
         cy.contains('Next').click()
         cy
-        .get('.form-text.help-block')
-        .should('contain', 'Address is missing')
+          .get('.form-text.help-block')
+          .should('contain', 'Address is missing')
       })
 
       it('cannot send funds to invalid address', () => {
@@ -36,19 +36,62 @@ describe('Transactions Test', () => {
         cy.contains('Equibit').click()
         cy.contains('Next').click()
         cy
-        .get('.form-text.help-block')
-        .should('contain', 'Invalid address')
+          .get('.form-text.help-block')
+          .should('contain', 'Invalid address')
     
         cy.contains('Bitcoin').click()
         cy.contains('Next').click()
         cy
-        .get('.form-text.help-block')
-        .should('contain', 'Invalid address')
+          .get('.form-text.help-block')
+          .should('contain', 'Invalid address')
       })
 
       it.only('can send EQB to valid address', function () {
         cy.get('input[placeholder="Paste address"]').type(this.users.validUsers[2].plainEQBaddress)
         cy.contains('Equibit').click()
+        cy
+          .get('input[type="number"]')
+          .type('.00001')
+        cy.contains('Next').click()
+
+        cy
+          .get('.modal-title')
+          .should('contain', 'Send Funds')
+        cy
+          .get('.input-value.break-word')
+          .should('contain', this.users.validUsers[2].plainEQBaddress)
+        cy
+          .get('.column-digital-currency-value')
+          .should('contain', '0.00001')
+        cy
+          .get('.btn.btn-primary')
+          .should('contain', 'Send')
+          .should('have.attr', 'on:click', 'send(@close)')
+          .click()
+        cy
+          .get('.dropdown-toggle.icon.icon-user')
+          .click()
+        cy
+          .contains('Transactions')
+          .should('be.visible')
+          .click()
+        cy.url().should('contain', '/transactions')
+        cy
+          .get('.panel-title')
+          .should('contain', 'All Transactions')
+        cy
+          .get('.selected.tx-in')
+          .should('contain', 'Transfer Out')
+          .should('contain', 'Equibit')
+        cy
+          .get('#column-cash-EQB')
+          .should('contain', '0.00002505')
+        
+      })
+
+      it('can send BTC to valid address', function () {
+        cy.get('input[placeholder="Paste address"]').type(this.users.validUsers[2].plainEQBaddress)
+        cy.contains('Bitcoin').click()
         cy.get('input[type="number"]').type(0.00001)
         cy.contains('Next').click()
       })
