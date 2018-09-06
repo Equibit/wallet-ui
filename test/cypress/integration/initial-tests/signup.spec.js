ab1b9bac29f69b9bfda34c4263aba573cae8436b
Elliot Tio <elliott@evenset.com>
2018-09-06 15:55:57 -0400

    lint

--- test/cypress/integration/initial-tests/signup.spec.js
+++ test/cypress/integration/initial-tests/signup.spec.js
@@ -1,101 +1,101 @@
 describe('Signup Test', () => {
-    beforeEach(() => {
-      cy.fixture('users').as('users')
-      cy.visit('/')
-      cy.get('input[type="password"]').type(Cypress.env('HTTP_PASSWORD'))
-      cy.get('button[type="submit"]').click()
-      cy.contains('Sign Up').click()
-      cy.url().should('contain', '/signup')
-    })
-  
-    it('greets user with Create an Account', () => {
-      cy.contains('h2', 'Create an Account')
-    })
+  beforeEach(() => {
+    cy.fixture('users').as('users')
+    cy.visit('/')
+    cy.get('input[type="password"]').type(Cypress.env('HTTP_PASSWORD'))
+    cy.get('button[type="submit"]').click()
+    cy.contains('Sign Up').click()
+    cy.url().should('contain', '/signup')
+  })
 
-    it('contains link to T&C', () => {
-        cy
+  it('greets user with Create an Account', () => {
+    cy.contains('h2', 'Create an Account')
+  })
+
+  it('contains link to T&C', () => {
+    cy
         .contains('Terms & Conditions')
         .click()
-        cy
+    cy
         .contains('h3', 'Terms & Conditions')
         .should('be.visible')
-    })
+  })
 
-    it('contains link to Privacy Policy', () => {
-        cy
+  it('contains link to Privacy Policy', () => {
+    cy
         .contains('Privacy Policy')
         .click()
-        cy
+    cy
         .contains('h3', 'Privacy Policy')
         .should('be.visible')
-    })
+  })
 
-    it('signup invalid temp password', function () {
-        cy
+  it('signup invalid temp password', function () {
+    cy
         .get('input[type="email"]')
         .type(this.users.signup.email)
-        cy
+    cy
         .get('input[type="checkbox"]')
         .check()
-        cy
+    cy
         .get('button[type="submit"]')
         .click()
 
-        cy.contains('p', 'Check your email to continue, and then head to login.')
-        cy
+    cy.contains('p', 'Check your email to continue, and then head to login.')
+    cy
         .contains('login')
         .should('have.attr', 'href', '/')
         .click()
-        cy.url().should('contain', '/')
+    cy.url().should('contain', '/')
 
-        cy
+    cy
         .get('input[type="email"')
         .should('have.value', this.users.signup.email)
         .get('input[type="password"')
         .type(this.users.signup.invalidpassword)
         .get('button[type="submit"]')
         .click()
-        
-        cy
+
+    cy
         .get('.alert')
         .should('contain', 'Invalid login.')
-    })
+  })
 
-    it('signup invalid new password', function () {
-        cy
+  it('signup invalid new password', function () {
+    cy
         .get('input[type="email"]')
         .type(this.users.signup.email)
-        cy
+    cy
         .get('input[type="checkbox"]')
         .check()
-        cy
+    cy
         .get('button[type="submit"]')
         .click()
 
-        cy.contains('p', 'Check your email to continue, and then head to login.')
-        cy
+    cy.contains('p', 'Check your email to continue, and then head to login.')
+    cy
         .contains('login')
         .should('have.attr', 'href', '/')
         .click()
-        cy.url().should('contain', '/')
+    cy.url().should('contain', '/')
 
-        cy
+    cy
         .get('input[type="email"')
         .should('have.value', this.users.signup.email)
         .get('input[type="password"')
         .type(this.users.signup.temppassword)
         .get('button[type="submit"]')
         .click()
-        cy.url().should('contain', 'change-password')
+    cy.url().should('contain', 'change-password')
 
-        cy
+    cy
         .contains('h2', 'Create a Password')
         .should('exist')
-        cy
+    cy
         .contains('p', 'Create a password for your account.')
         .should('exist')
 
-        cy
+    cy
         .get('input[type="password"]')
         .type(this.users.signup.invalidpassword)
         .get('[data-cy=password-strength]')
@@ -104,45 +104,45 @@ describe('Signup Test', () => {
         .should('contain', 'Save & Log In')
         .click()
 
-        cy.url().should('contain', 'change-password')
-        cy.contains('Password too weak').should('be.visible')
-    })
+    cy.url().should('contain', 'change-password')
+    cy.contains('Password too weak').should('be.visible')
+  })
 
-    it('signup flow', function () {
-        cy
+  it('signup flow', function () {
+    cy
         .get('input[type="email"]')
         .type(this.users.signup.email)
-        cy
+    cy
         .get('input[type="checkbox"]')
         .check()
-        cy
+    cy
         .get('button[type="submit"]')
         .click()
 
-        cy.contains('p', 'Check your email to continue, and then head to login.')
-        cy
+    cy.contains('p', 'Check your email to continue, and then head to login.')
+    cy
         .contains('login')
         .should('have.attr', 'href', '/')
         .click()
-        cy.url().should('contain', '/')
+    cy.url().should('contain', '/')
 
-        cy
+    cy
         .get('input[type="email"')
         .should('have.value', this.users.signup.email)
         .get('input[type="password"')
         .type(this.users.signup.temppassword)
         .get('button[type="submit"]')
         .click()
-        cy.url().should('contain', 'change-password')
+    cy.url().should('contain', 'change-password')
 
-        cy
+    cy
         .contains('h2', 'Create a Password')
         .should('exist')
-        cy
+    cy
         .contains('p', 'Create a password for your account.')
         .should('exist')
 
-        cy
+    cy
         .get('input[type="password"]')
         .type(this.users.signup.password)
         .get('[data-cy=password-strength]')
@@ -151,7 +151,6 @@ describe('Signup Test', () => {
         .should('contain', 'Save & Log In')
         .click()
 
-        cy.url().should('contain', 'portfolio')
-    })
+    cy.url().should('contain', 'portfolio')
+  })
 })
-  
