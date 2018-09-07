5a6f14856a05e4cc3a0f065b6326fcd623e2a220
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-09-07 15:46:19 -0400

    email verification getting close

--- test/cypress/integration/preferences-page/verify-email.spec.js
+++ test/cypress/integration/preferences-page/verify-email.spec.js
@@ -2,6 +2,26 @@
 
 describe('Verify Email Test', () => {
   let user
+  const goToPreferences = function () {
+    cy
+      .get('[data-cy=userDropdown]')
+      .should('have.attr', 'href', '#')
+      .click()
+    cy
+      .get('[data-cy=userPreferences]')
+      .should('have.attr', 'href', '/preferences')
+      .click()
+  }
+  const openDialog = function () {
+    cy
+      .get('[data-cy=edit-email-button]')
+      .click()
+  }
+  const resendVerification = function () {
+    cy
+      .get('[data-cy=twofactor-tryagain-button]')
+      .click({ force: true })
+  }
 
   beforeEach(function () {
     cy.loginQA()
@@ -9,12 +29,41 @@ describe('Verify Email Test', () => {
       .fixture('users')
       .as('users')
       .then((users) => {
-        user = users.validUsers[0]
+        user = users.twoStepVerification
         cy.login(user)
       })
   })
 
-  it('', function () {
-    
+  it('email verification is openable', function () {
+    goToPreferences()
+    openDialog()
+    cy
+      .get('modal-authentication')
+      .should('exist')
+  })
+
+  it('incorrect verification code dos not allow email change', function () {
+    goToPreferences()
+    openDialog()
+    cy
+      .get('code-input input[type=password]')
+      .type(user.invalidTwoFactorCode)
+    cy
+      .get('[data-cy=verify-auth-button]')
+      .click()
+    cy
+      .get('validation-message')
+      .should('not.be.empty')
+  })
+
+  it('correct verification code allows email change', function () {
+    goToPreferences()
+    openDialog()
+    cy
+      .get('code-input input[type=password]')
+      .type(user.twoFactorCode)
+    cy
+      .get('[data-cy=verify-auth-button]')
+      .click()
   })
 })
