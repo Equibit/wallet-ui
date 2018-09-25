3ad7617297c3b82aa7754f27d70dd425f0b4b3db
Elliot Tio <elliott@evenset.com>
2018-09-25 09:44:39 -0400

    log portfolio address

--- test/cypress/integration/ui-tests/pages/portfolio.spec.js
+++ test/cypress/integration/ui-tests/pages/portfolio.spec.js
@@ -38,7 +38,8 @@ describe('Portfolio Test', () => {
   })
 
   it('user has EQB', function () {
-    cy.login(this.users.validUsers[3])
+    cy.login(this.users.validUsers[0])
+    cy.logAddresses()
     cy.url().should('contain', '/portfolio')
     cy
       .get('[data-cy=loading-overlay]')
