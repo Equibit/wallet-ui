47a38deae32c529d152969850fb80aaaa7f0c455
Elliot Tio <elliott@evenset.com>
2018-10-04 11:46:17 -0400

    wip equibit-page tests

--- test/cypress/integration/ui-tests/pages/order-book.spec.js
+++ test/cypress/integration/ui-tests/pages/order-book.spec.js
@@ -2,12 +2,16 @@
 import goToEquibitPage from '../../../support/utils/trade-helpers'
 
 describe('Equibit Page Test', () => {
+    before(() => {
+      cy.clearOrdersAndOffers()        
+      cy.addOrders('many', 12)
+    })
+
     beforeEach(() => {
-      cy.clearOrdersAndOffers()
       cy.loginQA()
       cy.fixture('users').as('users')
     })
-    describe('Without Logging In', () => {
+    describe('Logged Out', () => {
         beforeEach(() => {
             cy.visit('/equibit')
         })
@@ -26,6 +30,7 @@ describe('Equibit Page Test', () => {
 
         it('cannot add sell or buy order', () => {
             cy.contains('Add Sell Order')
+              .scrollIntoView()
               .click()
 
             cy.contains('Log in to continue.')
@@ -36,9 +41,11 @@ describe('Equibit Page Test', () => {
               .and('have.attr', 'href', '/login')
 
             cy.contains('Add Sell Order')
+              .scrollIntoView()
               .click()
 
             cy.contains('Add Buy Order')
+              .scrollIntoView()
               .click()
 
             cy.contains('Log in to continue.')
@@ -49,10 +56,20 @@ describe('Equibit Page Test', () => {
               .and('have.attr', 'href', '/login')
         })
 
-        describe('With Orders', () => {
-            beforeEach(() => {
-                cy.addOrders('many')
-            })
+        it('load more button does not show when less than 22 orders', () => {
+            cy.get('[data-cy=order-loading-indicator]').should('not.be.visible')
+            cy.get('[data-cy=load-more-sell]')
+                .should('not.be.visible')
+            cy.get('[data-cy=load-more-buy]')
+                .should('not.be.visible')
+        })
+
+        it('load more button shows when more than 23 orders', () => {
+            cy.get('[data-cy=order-loading-indicator]').should('not.be.visible')
+            cy.get('[data-cy=load-more-sell]')
+                .should('be.visible')
+            cy.get('[data-cy=load-more-buy]')
+                .should('be.visible')
         })
     })
     describe('Logged In', () => {
